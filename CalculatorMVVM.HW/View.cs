e877c25b0b52f1e4003479c71c52bb8ceb93b7e9
F1eshka <144143888+F1eshka@users.noreply.github.com>
2025-01-11 13:02:16 +0300

    Add files via upload

---
+++ CalculatorMVVM.HW/View.cs
@@ -0,0 +1,54 @@
+﻿using CalculatorMVVM.HW.Model;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+public class Class1
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private readonly CalculatorModel calculatorModel = new();
+
+        public string Display => calculatorModel.Display;
+        public string FullExpression => calculatorModel.FullExpression;
+
+        public ICommand NumberCommand => new RelayCommand(param =>
+        {
+            calculatorModel.AppendNumber(param?.ToString() ?? string.Empty);
+            UpdateProperties();
+        });
+
+        public ICommand ClearCommand => new RelayCommand(_ =>
+        {
+            calculatorModel.Clear();
+            UpdateProperties();
+        });
+
+        public ICommand OperatorCommand => new RelayCommand(param =>
+        {
+            if (param is string operatorSymbol)
+            {
+                calculatorModel.SetOperator(operatorSymbol);
+                UpdateProperties();
+            }
+        });
+
+        public ICommand EqualsCommand => new RelayCommand(_ =>
+        {
+            calculatorModel.Calculate();
+            UpdateProperties();
+        });
+
+        private void UpdateProperties()
+        {
+            OnPropertyChanged(nameof(Display));
+            OnPropertyChanged(nameof(FullExpression));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
